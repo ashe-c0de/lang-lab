fb8e2c9de1b5cdd024687a2d208e8aff10efd2ca
Ashe <ashet@qq.com>
2026-03-01 15:03:44 +0800

    Add basic map operations in map.go
    Implement basic map operations including create, write, read, delete, and iterate.

---
+++ golang/map/map.go
@@ -0,0 +1,29 @@
+func main() {
+	// 创建map
+	myMap := make(map[string]any)
+
+	// 写
+	myMap["a"] = 1
+	myMap["b"] = 2
+	myMap["c"] = 3
+
+	// 读
+	v1, ok := myMap["c"]
+	if ok {
+		fmt.Println(v1)
+	}
+
+	v2, ok := myMap["d"]
+	if ok {
+		fmt.Println(v2)
+	}
+
+	// 删
+	delete(myMap, "c")
+	delete(myMap, "d")
+
+	// 遍历
+	for k, v := range myMap {
+		fmt.Println(k, v)
+	}
+}
