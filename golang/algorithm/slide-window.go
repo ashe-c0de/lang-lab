9188660454f9d3da90219b01392ea049ee2fcf9a
Ashe <ashet@qq.com>
2026-03-03 16:58:53 +0800

    Update slide-window.go

--- golang/algorithm/slide-window.go
+++ golang/algorithm/slide-window.go
@@ -1,3 +1,7 @@
+package main
+
+import "fmt"
+
 /*
 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
 
@@ -9,7 +13,7 @@
 */
 func main() {
 	var str = "abcabcbb"
-	var str2 = "abcabcfbb"
+	var str2 = "你好世界红豆泥世界"
 	var str3 = ""
 	fmt.Println(test(str))
 	fmt.Println(test(str2))
@@ -17,17 +21,18 @@ func main() {
 }
 
 func test(str string) int {
-	m := make(map[byte]int)
+	runes := []rune(str)
+	m := make(map[rune]int)
 	left := 0
-	res := 0
+	result := 0
 	// 滑动窗口算法的核心就是，右指针给窗口扩容，直至抵达扩容限制条件或抵达边界；左指针则是给窗口缩容，以释放限制条件的约束，保证窗口继续向边界移动。
-	for right := 0; right < len(str); right++ {
-		if index, ok := m[str[right]]; ok && index >= left {
-			left = index + 1
+	for right := 0; right < len(runes); right++ {
+		if v, ok := m[runes[right]]; ok && v >= left {
+			left = v + 1
 		}
-		m[str[right]] = right
-		res = max(res, right-left+1)
+		m[runes[right]] = right
+		result = max(result, right-left+1)
 	}
 
-	return res
+	return result
 }
